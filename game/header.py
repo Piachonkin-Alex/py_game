4b7ee262d15516f83a1069fd0c87c3eb969d8483
Piachonkin-Alex <pechenkin.aa@phystech.edu>
2020-04-05 01:45:34 +0300

    first version

--- game/header.py
+++ game/header.py
@@ -3,18 +3,142 @@ import random
 
 
 class Barrier:
-    def __init__(self, x, y, width, height, movement):
+    def __init__(self, x, y, width, movement, img):
         self.x = x
         self.y = y
         self.width = width
-        self.height = height
         self.movement = movement
+        self.image = img
 
     def move(self, our_display, display_width):
         if self.x >= -self.width:
-            pygame.draw.rect(our_display, (85, 107, 47), (self.x, self.y, self.width, self.height))
+            our_display.blit(self.image, (self.x, self.y))
             self.x -= self.movement
             return True
         else:
-            self.x = display_width + 120 + random.randrange(-100, 70)
             return False
+
+    def remove_right(self, new_x, new_y, new_width, new_img):
+        self.x = new_x
+        self.y = new_y
+        self.width = new_width
+        self.image = new_img
+
+
+barrier_images = []
+barrier_images.append(pygame.image.load('barrier0.png'))
+barrier_images.append(pygame.image.load('barrier1.png'))
+barrier_images.append(pygame.image.load('barrier2.png'))
+barrier_images_size = [[37, 618], [69, 657], [40, 628]]
+
+
+def create_barriers(barrier_list, display_width):
+    dist = [100, 500, 900]
+    for i in range(3):
+        choice = random.randrange(0, 3)
+        img = barrier_images[choice]
+        width = barrier_images_size[choice][0]
+        height = barrier_images_size[choice][1]
+        barrier_list.append(Barrier(display_width + dist[i], height, width, 5.2, img))
+
+
+def find_distance(barrier_list, display_width):
+    right_point = max(barrier_list[0].x, barrier_list[1].x, barrier_list[2].x)
+    distance = 0
+    if right_point < display_width:
+        distance = display_width
+        if distance - right_point < 250:
+            distance += 300
+    else:
+        distance = right_point
+    choice_of_dist = random.randrange(0, 12)
+    if choice_of_dist < 4:
+        distance += random.randrange(43, 56)
+    else:
+        distance += random.randrange(280, 450)
+    return distance
+
+
+def draw_barries(barrier_list, our_display, display_width):
+    for barrier in barrier_list:
+        checker = barrier.move(our_display, display_width)
+        if not checker:
+            distance = find_distance(barrier_list, display_width)
+
+            choice = random.randrange(0, 3)
+            img = barrier_images[choice]
+            width = barrier_images_size[choice][0]
+            height = barrier_images_size[choice][1]
+
+            barrier.remove_right(distance, height, width, img)
+
+
+def print_text(text: str, cord_x, cord_y, font_type, font_size, our_display):
+    font = pygame.font.Font(font_type, font_size)
+    text = font.render(text, True, (0, 0, 0))
+    our_display.blit(text, (cord_x, cord_y))
+
+
+def pause(our_display, clock):
+    pause_ind = True
+    while pause_ind:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                quit()
+        font_type = 'ofont.ru_EE-Bellflower.ttf'
+        print_text('Press ENTER to continue', 300, 300, font_type, 37, our_display)
+
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_RETURN]:
+            pause_ind = False
+        pygame.display.update()
+        clock.tick(15)
+
+
+chel_images = []
+chel_images.append(pygame.image.load('chel1.png'))
+chel_images.append(pygame.image.load('chel2.png'))
+chel_images.append(pygame.image.load('chel3.png'))
+
+image_counter = 0
+
+
+def draw_chel(our_display, cord_x, cord_y):
+    global image_counter
+    if image_counter == 21:
+        image_counter = 0
+    our_display.blit(chel_images[image_counter // 7], (cord_x, cord_y))
+    image_counter += 1
+
+
+def check_conflict(barrier_list, char_x, char_y, char_height, char_width):
+    for barrier in barrier_list:
+        if char_y + char_height >= barrier.y:
+            if barrier.x <= char_x <= barrier.x + barrier.width:
+                return True
+            elif barrier.x <= char_x + char_width - 35 <= barrier.x + barrier.width:
+                return True
+    return False
+
+
+def end_game(our_display, clock, score, prev_max_score):
+    max_score = max(score, prev_max_score)
+    end_game_ind = True
+    while end_game_ind:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                quit()
+        font_type = 'ofont.ru_EE-Bellflower.ttf'
+        first_text = 'Press ENTER to continue, esc to exit'
+        print_text(first_text, 200, 300, font_type, 37, our_display)
+        second_text = 'Max score is ' + str(int(max_score))
+        print_text(second_text, 360, 360, font_type, 37, our_display)
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_RETURN]:
+            return True
+        if keys[pygame.K_ESCAPE]:
+            return False
+        pygame.display.update()
+        clock.tick(15)
--- game/run.py
+++ game/run.py
@@ -4,6 +4,8 @@ import header
 
 pygame.init()
 
+font_type = 'ofont.ru_EE-Bellflower.ttf'
+
 clock = pygame.time.Clock()
 
 display_width = 1024
@@ -12,79 +14,73 @@ display_height = 768
 our_display = pygame.display.set_mode((display_width, display_height))
 pygame.display.set_caption('Subway Surf 2D')
 
-char_width = 70
-char_height = 110
+char_width = 111
+char_height = 139
 
 char_x = 3 * display_width // 10
 char_y = display_height - 60 - char_height
 
 do_jump = False
-jump_count = 35
+jump_count = 33
+
+score = 0
+max_score = 0
+
+
+def jump():
+    global jump_count, do_jump, char_y
+    if jump_count >= -33:
+        char_y -= jump_count / 1.7
+        jump_count -= 1
+    else:
+        jump_count = 33
+        do_jump = False
+
+
+def count_score():
+    global score
+    score += 1 / 180
 
 
 def run_game():
     global do_jump
     game = True
     barrier_list = []
-    create_barriers(barrier_list)
-    background = pygame.image.load(r'/home/sanches/github/py_game/game/background.png').convert()
+    header.create_barriers(barrier_list, display_width)
+    background = pygame.image.load(r'background.png').convert()
     while game:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 quit()
+
         keys = pygame.key.get_pressed()
+
         if keys[pygame.K_SPACE]:
             do_jump = True
+
+        if keys[pygame.K_ESCAPE]:
+            header.pause(our_display, clock)
         if do_jump:
             jump()
+        count_score()
         our_display.blit(background, (0, 0))
-        draw_barries(barrier_list)
-        pygame.draw.rect(our_display, (211, 214, 22), (char_x, char_y, char_width, char_height))
+        header.print_text('score: ' + str(int(score)), 45, 22, font_type, 35, our_display)
+        header.draw_barries(barrier_list, our_display, display_width)
+        header.draw_chel(our_display, char_x, char_y)
+        if header.check_conflict(barrier_list, char_x, char_y, char_width, char_height):
+            game = False
         pygame.display.update()
-        clock.tick(120)
+        clock.tick(70)
+    return header.end_game(our_display, clock, score, max_score)
 
 
-def create_barriers(barrier_list):
-    barrier_list.append(header.Barrier(display_width + 25, display_height - 140, 25, 80, 5.3))
-    barrier_list.append(header.Barrier(display_width + 400, display_height - 120, 30, 60, 5.3))
-    barrier_list.append(header.Barrier(display_width + 840, display_height - 110, 35, 50, 5.3))
-    barrier_list.append(header.Barrier(display_width + 1280, display_height - 150, 20, 90, 5.3))
-
-
-def draw_barries(barrier_list):
-    for barrier in barrier_list:
-        checker = barrier.move(our_display, display_width)
-        if not checker:
-            distance = find_distance(barrier_list)
-            barrier.x = distance
-
-
-def find_distance(barrier_list):
-    right_point = max(barrier_list[0].x, barrier_list[1].x, barrier_list[2].x, barrier_list[0].x)
-    distance = 0
-    if right_point < display_width:
-        distance = display_width
-        if distance - right_point < 150:
-            distance += 320
-    else:
-        distance = right_point
-    choice_of_dist = random.randrange(0, 12)
-    if choice_of_dist < 2:
-        distance += random.randrange(5, 10)
-    else:
-        distance += random.randrange(400, 500)
-    return distance
-
-
-def jump():
-    global jump_count, do_jump, char_y
-    if jump_count >= -35:
-        char_y -= jump_count / 1.5
-        jump_count -= 1
-    else:
-        jump_count = 35
-        do_jump = False
-
-
-run_game()
+while run_game():
+    if score > max_score:
+        max_score = score
+    do_jump = False
+    jump_count = 33
+    score = 0
+    char_y = display_height - 60 - char_height
+pygame.quit()
+quit()

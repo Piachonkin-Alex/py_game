0af52354b9a9dd8d4be8334b0ddc96ca5cda02dd
Piachonkin-Alex <pechenkin.aa@phystech.edu>
2020-04-07 00:10:48 +0300

    first request

--- game/header.py
+++ game/header.py
@@ -2,8 +2,14 @@ import pygame
 import random
 
 
-class Barrier:
-    def __init__(self, x, y, width, movement, img) -> None:
+# тут хотелось бы дать общий комментарий по поводу вообще работы с pygame. Минус этой штуки в том, что очень часто
+# требуется работать с глобальными переменными. Поэтому некоторые функции в другой файл не переносятся.
+# к примеру jump. В этом файле вы будете часто видеть, что я передаю всегда свой дисплей и его размеры, и поэтому
+# часто в функции получается много исходных аргументов. Поэтому для меня было изначально ужобнее писыть все
+# одном файле. Но я все-таки реализацию сложных вычислительных вешей постарался перенести сюда.
+
+class Barrier:  # класс барьера
+    def __init__(self, x, y, width, movement, img) -> None:  # инициализация по заданным координатам и размеру и img
         """Barrier initialization"""
         self.x = x
         self.y = y
@@ -11,7 +17,7 @@ class Barrier:
         self.movement = movement
         self.image = img
 
-    def move(self, our_display) -> None:
+    def move(self, our_display) -> None:  # движение барьеров. Возвращает True, если барьер ушел за дисплей слева
         """Barrier motion"""
         if self.x >= -self.width:
             our_display.blit(self.image, (self.x, self.y))
@@ -20,7 +26,7 @@ class Barrier:
         else:
             return False
 
-    def remove_right(self, new_x, new_y, new_width, new_img) -> None:
+    def remove_right(self, new_x, new_y, new_width, new_img) -> None:  # мгновенное перемещение барьера вправо
         """replace barrier and change"""
         self.x = new_x
         self.y = new_y
@@ -28,29 +34,29 @@ class Barrier:
         self.image = new_img
 
 
-barrier_images = []
-barrier_images.append(pygame.image.load('barrier0.png'))
-barrier_images.append(pygame.image.load('barrier1.png'))
-barrier_images.append(pygame.image.load('barrier2.png'))
-barrier_images_size = [[37, 618], [69, 657], [40, 628]]
+barrier_images = []  # массив картинок барьеров
+barrier_images.append(pygame.image.load('high.png'))
+barrier_images.append(pygame.image.load('middle.png'))
+barrier_images.append(pygame.image.load('wide.png'))
+barrier_images_size = [[47, 608], [58, 628], [85, 645]]
 
 
-def create_barriers(barrier_list, display_width) -> None:
+def create_barriers(barrier_list, display_width) -> None:  # создание барьеров
     """Initialization of game Barriers"""
-    dist = [100, 500, 900]
+    dist = [100, 500, 900]  # начальная их координата
     for i in range(3):
         choice = random.randrange(0, 3)
         img = barrier_images[choice]
         width = barrier_images_size[choice][0]
-        height = barrier_images_size[choice][1]
+        height = barrier_images_size[choice][1]  # сообственно создание
         barrier_list.append(Barrier(display_width + dist[i], height, width, 5.2, img))
 
 
 def find_distance(barrier_list, display_width) -> int:
+    # здесь производится случайный выбор доступного расстояния для крайних справа барьеров
     """calculate future distance between barriers"""
-    right_point = max(barrier_list[0].x, barrier_list[1].x, barrier_list[2].x)
-    distance = 0
-    if right_point < display_width:
+    right_point = max(barrier_list[0].x, barrier_list[1].x, barrier_list[2].x)  # координата самого правого барьера
+    if right_point < display_width:  # проверка на то, что самый правый барьер находится за размером дисплеея
         distance = display_width
         if distance - right_point < 250:
             distance += 300
@@ -60,66 +66,69 @@ def find_distance(barrier_list, display_width) -> int:
     if choice_of_dist < 6:
         distance += random.randrange(43, 56)
     else:
-        distance += random.randrange(280, 450)
+        distance += random.randrange(280, 450)  # здесь происходит случайный выбор расстояния между барьерами
+        # варианта 2: близко к друг другу, чтобы сразу перепрыгнуть 2, иои н нормальном расстоянии
     return distance
 
 
 def draw_barries(barrier_list, our_display, display_width) -> None:
+    # прорисовка барьеров на дисплей
     """draw barriers on display"""
     for barrier in barrier_list:
         checker = barrier.move(our_display)
-        if not checker:
+        if not checker:  # если барьер ушел из дисплея слева, мы его перемещаем вправо согласно подсчету предыдущей ф-и
             distance = find_distance(barrier_list, display_width)
 
             choice = random.randrange(0, 3)
             img = barrier_images[choice]
             width = barrier_images_size[choice][0]
-            height = barrier_images_size[choice][1]
+            height = barrier_images_size[choice][1]  # случайный выбор новго вида барьера
 
-            barrier.remove_right(distance, height, width, img)
+            barrier.remove_right(distance, height, width, img)  # перемещение
 
 
-def print_text(text: str, cord_x, cord_y, font_type, font_size, our_display) -> None:
+def print_text(text: str, cord_x, cord_y, font_type, font_size, our_display) -> None:  # ечать текста на дисплее
     """print text on display"""
     font = pygame.font.Font(font_type, font_size)
     text = font.render(text, True, (0, 0, 0))
     our_display.blit(text, (cord_x, cord_y))
 
 
-def pause(our_display, clock) -> None:
+def pause(our_display, clock) -> None:  # пауза
     """game pause"""
     pause_ind = True
-    pygame.mixer.music.pause()
+    pygame.mixer.music.pause()  # пауза в музыке
 
     while pause_ind:
-        for event in pygame.event.get():
+        for event in pygame.event.get():  # нажали крестик -- вышли
             if event.type == pygame.QUIT:
                 pygame.quit()
                 quit()
 
-        font_type = 'ofont.ru_EE-Bellflower.ttf'
+        font_type = 'ofont.ru_EE-Bellflower.ttf'  # прописовка надписи при паузе
         print_text('Press ENTER to continue', 300, 300, font_type, 37, our_display)
 
-        keys = pygame.key.get_pressed()
-        if keys[pygame.K_RETURN]:
+        keys = pygame.key.get_pressed()  # считывание нажатых клавиш
+        if keys[pygame.K_RETURN]:  # нажали ENTER -- продолжили игру
             pause_ind = False
 
-        pygame.display.update()
+        pygame.display.update()  # обновление дисплея
         clock.tick(15)
 
-    pygame.mixer.music.unpause()
+    pygame.mixer.music.unpause()  # убираем паузу с музыки
 
 
-chel_images = []
+chel_images = []  # а тут длина строки по pep-8 не дает избежать варнинга(
 chel_images.append(pygame.image.load('chel1.png'))
-chel_images.append(pygame.image.load('chel2.png'))
+chel_images.append(pygame.image.load('chel2.png'))  # загрузка картинок для анимации персонажа
 chel_images.append(pygame.image.load('chel3.png'))
 
-image_counter = 0
+image_counter = 0  # cчетчик смены картинки в анимации
 
 
 def draw_char(our_display, cord_x, cord_y) -> None:
     """drawing character"""
+    # прописовка персонажа на дисплее
     global image_counter
     if image_counter == 18:
         image_counter = 0
@@ -128,10 +137,12 @@ def draw_char(our_display, cord_x, cord_y) -> None:
 
 
 def check_conflict(barrier_list, char_x, char_y, char_height, char_width) -> bool:
+    # проверка, врезались мы или нет. тут чистая математика +- некоторые константы,
+    # так как мы управлчем не прямоугольниками
     """check crash"""
     for barrier in barrier_list:
         if char_y + char_height >= barrier.y:
-            if barrier.x <= char_x + 15 <= barrier.x + barrier.width:
+            if barrier.x <= char_x + 5 <= barrier.x + barrier.width:
                 return True
             elif barrier.x <= char_x + char_width - 60 <= barrier.x + barrier.width:
                 return True
@@ -139,23 +150,25 @@ def check_conflict(barrier_list, char_x, char_y, char_height, char_width) -> boo
 
 
 def end_game(our_display, clock, score, prev_max_score) -> None:
+    # завершение игры.
     """end game screen"""
     max_score = max(score, prev_max_score)
     end_game_ind = True
+
     while end_game_ind:
-        for event in pygame.event.get():
+        for event in pygame.event.get():  # нажали крестик -- вышли
             if event.type == pygame.QUIT:
                 pygame.quit()
                 quit()
-        font_type = 'ofont.ru_EE-Bellflower.ttf'
-        first_text = 'Press ENTER to continue, esc to exit'
-        print_text(first_text, 200, 300, font_type, 37, our_display)
-        second_text = 'Max score is ' + str(int(max_score))
+        font_type = 'ofont.ru_EE-Bellflower.ttf'  # шрифт
+        first_text = 'Press ENTER to continue, esc to exit'  # надпись при прогрыше
+        print_text(first_text, 200, 300, font_type, 37, our_display)  # ее вывод на экран
+        second_text = 'Max score is ' + str(int(max_score))  # вывод макс. счета, полученного после открытия игры
         print_text(second_text, 360, 360, font_type, 37, our_display)
         keys = pygame.key.get_pressed()
-        if keys[pygame.K_RETURN]:
+        if keys[pygame.K_RETURN]:  # нажали ENTER -- начали сначала
             return True
-        if keys[pygame.K_ESCAPE]:
+        if keys[pygame.K_ESCAPE]:  # нажали ESCAPE -- вышли из игры
             return False
-        pygame.display.update()
+        pygame.display.update()  # обновление дисплея
         clock.tick(15)
--- game/run.py
+++ game/run.py
@@ -4,35 +4,35 @@ import header
 
 pygame.init()
 
-pygame.mixer.music.load('background.mp3')
+pygame.mixer.music.load('background.mp3')  # загрузка музыки
 
-font_type = 'ofont.ru_EE-Bellflower.ttf'
+font_type = 'ofont.ru_EE-Bellflower.ttf'  # загрузка шрифта
 
-clock = pygame.time.Clock()
+clock = pygame.time.Clock()  # счетчик кадров и времени
 
 display_width = 1024
-display_height = 768
+display_height = 768  # размеры дисплея
 
-our_display = pygame.display.set_mode((display_width, display_height))
+our_display = pygame.display.set_mode((display_width, display_height))  # создание дисплея
 pygame.display.set_caption('Subway Surf 2D')
 
 char_width = 111
-char_height = 139
+char_height = 139  # размеры героя
 
 char_x = 3 * display_width // 10
-char_y = display_height - 60 - char_height
+char_y = display_height - 60 - char_height  # координата местоположения героя
 
-do_jump = False
-jump_count = 33
+do_jump = False  # индикатор прыжка
+jump_count = 33  # счетчик смены координаты прыжка прыжка
 
-score = 0
-max_score = 0
+score = 0  # счет
+max_score = 0  # максимальный счет при запуске окна
 
 
 def jump() -> None:
     """Do jump"""
     global jump_count, do_jump, char_y
-    if jump_count >= -33:
+    if jump_count >= -33:  # пока счетчик не достиг -33 меняем координату героя
         char_y -= jump_count / 1.7
         jump_count -= 1
     else:
@@ -42,59 +42,66 @@ def jump() -> None:
 
 def count_score() -> None:
     """Counting score"""
-    global score
+    global score  # при вызове функции немного увеличиваем счет
     score += 1 / 180
 
 
-def run_game() -> bool:
+def run_game() -> bool:  # цикл игры. Сама механика игры -- это цикл, который прерывается только при выходе
     """Game cycle process"""
     global do_jump
 
-    pygame.mixer.music.play(-1)
-    background = pygame.image.load(r'background.png').convert()
+    pygame.mixer.music.play(-1)  # проигрывание музыки
+    background = pygame.image.load(r'background.png').convert()  # загрузка изображения фона
     game = True
 
     barrier_list = []
-    header.create_barriers(barrier_list, display_width)
+    header.create_barriers(barrier_list, display_width)  # создание препядствий
     while game:
         for event in pygame.event.get():
-            if event.type == pygame.QUIT:
+            if event.type == pygame.QUIT:  # выход из программы при нажатии крестика
                 pygame.quit()
                 quit()
 
-        keys = pygame.key.get_pressed()
+        keys = pygame.key.get_pressed()  # считывание нажатых клавиш в данный момент
 
-        if keys[pygame.K_SPACE]:
+        if keys[pygame.K_SPACE]:  # нажали пробел -- прыжок
             do_jump = True
 
-        if keys[pygame.K_ESCAPE]:
+        if keys[pygame.K_ESCAPE]:  # нажали ESCAPE -- пауза
             header.pause(our_display, clock)
 
-        if do_jump:
+        if do_jump:  # прыжок, если горит индикатор прыжка
             jump()
 
-        count_score()
+        count_score()  # счет очков не забываем
 
-        our_display.blit(background, (0, 0))
-        header.print_text('score: ' + str(int(score)), 45, 22, font_type, 35, our_display)
-        header.draw_barries(barrier_list, our_display, display_width)
-        header.draw_char(our_display, char_x, char_y)
+        our_display.blit(background, (0, 0))  # заливка фона на дисплей
+        header.print_text('score: ' + str(int(score)), 45, 22, font_type, 35, our_display)  # табло очков на фоне
+        header.draw_barries(barrier_list, our_display, display_width)  # прорисовка барьеров
+        header.draw_char(our_display, char_x, char_y)  # прорисовка персонажа
 
         if header.check_conflict(barrier_list, char_x, char_y, char_width, char_height):
-            pygame.mixer.music.stop()
-            game = False
+            # проверка на то, что не врезались
+            pygame.mixer.music.stop()  # остановка музыки
+            game = False  # выход из цикла игры
 
-        pygame.display.update()
-        clock.tick(70)
+        pygame.display.update()  # обновление дисплея
+        clock.tick(70)  # частота
     return header.end_game(our_display, clock, score, max_score)
+    # возвращаем True или False, в зависимости от того, хотим ли еще играть
 
 
-while run_game():
-    if score > max_score:
+while run_game():  # пока запускаем игру
+    if score > max_score:  # меняем максимальный счет, если это надо
         max_score = score
     do_jump = False
     jump_count = 33
     score = 0
-    char_y = display_height - 60 - char_height
+    char_y = display_height - 60 - char_height  # меняем нужные переменные на начальные условия
 pygame.quit()
 quit()
+
+# потенциал развития -- огромен. как в плане улучшения самой игры, например реализации столкновений
+# -- они тут не идеальны, так и в плане удобства -- начальное меню, таблица рекордов и тд.
+# возможно при повторном отправлении что-то добавится)
+
